585fc8fab2ba5f01ef0c3cb7140430237a6973a4
Y. C. Zhang <b04902047@csie.ntu.edu.tw>
2021-03-09 11:42:58 +0800

    apps/reviewer: app.tsx

--- libs/ui/src/ExportList/ExportList.tsx
+++ libs/ui/src/ExportList/ExportList.tsx
@@ -21,13 +21,17 @@ export interface ExportListProps {
 export function ExportList(props: ExportListProps) {
   let style: CSSProperties = {
     backgroundColor: "#f7f7f7",
+    border: '2px solid #E4E4E4',
+    borderRight: 'none',
     paddingTop: 5,
-    paddingLeft: 37,
-    paddingBottom: 26,
+    paddingBottom: 16,
     overflow: "auto",
     whiteSpace: "nowrap",
     ...props.style
   };
+  let modelsStyle: CSSProperties = {
+    marginLeft: 37
+  }
   let modelIndices: number[] = Array.from(props.reviewItem.models.keys());
   let models: Map<number, ReviewModel> = props.reviewItem.models;
   let modelStyle: CSSProperties = {
@@ -35,19 +39,21 @@ export function ExportList(props: ExportListProps) {
   };
   return (
     <div style={style}>
-      {modelIndices.map((modelIndex => {
-        return (
-          <div
-            key={modelIndex}
-            style={modelStyle}
-          >
-            <ExportingModel
-              reviewModel={models.get(modelIndex)}
-              selectedPageIndex={(props.selectedModelIndex === modelIndex)? props.selectedPageIndex : undefined}
-            />
-          </div>
-        )
-      }))}
+      <div style={modelsStyle}>
+        {modelIndices.map((modelIndex => {
+          return (
+            <div
+              key={modelIndex}
+              style={modelStyle}
+            >
+              <ExportingModel
+                reviewModel={models.get(modelIndex)}
+                selectedPageIndex={(props.selectedModelIndex === modelIndex)? props.selectedPageIndex : undefined}
+              />
+            </div>
+          )
+        }))}
+      </div>
     </div>
   );
 };
