c95ff5fa2b759fef4a47dc330bf657a0ef8ffb4d
黃韻儒 <lalame888@gmail.com>
2021-03-09 10:07:10 +0800

    merge ImportList

--- libs/ui/src/ImportList/ImportList.tsx
+++ libs/ui/src/ImportList/ImportList.tsx
@@ -3,6 +3,8 @@ import './ImportList.module.css';
 import {UploadFileStatus} from '@gainhow-review/data'
 import ImportedFile from './imported-file/ImportedFile';
 import searchBarIcon from '../Icon/SearchBarIcon.svg';
+import FoldIcon from '../Icon/DoubleLeftIcon.svg';
+import UnfoldIcon from '../Icon/DoubleRightIcon.svg';
 /* eslint-disable-next-line */
 export interface ImportListProps {
   files: Array<UploadFileStatus>
@@ -13,25 +15,30 @@ export interface ImportListProps {
 export { ImportedFile };
 
 export function ImportList(props: ImportListProps) {
-  const [searchBarValue, setSearchBarValue] = useState('');
-
+  const [searchBarValue, setSearchBarValue] = useState<string>('');
+  const [isOpened, setisOpen] = useState<boolean>(true);
   function searchBarInputChangeHandler(event: React.ChangeEvent<HTMLInputElement>): void {
     setSearchBarValue(event.target.value);
   }
+  let foldIcon = (isOpened)? FoldIcon: UnfoldIcon ;
   const style: CSSProperties = {
     background: '#F7F7F7 0% 0% no-repeat padding-box',
     border: '2px solid #E4E4E4'
   }
   const toggleDivStyle: CSSProperties = {
-    
     borderBottom:'2px solid #E4E4E4',
-    height: '21px'
+    height: '21px',
+    display: 'flex',
+  }
+  const toggleIconStyle: CSSProperties = {
+    height: '16px',
   }
   const importListStyle: CSSProperties = {
     width: '240px',
     height: 'calc(100vh - 175px)',
     margin: '0px',
     padding: '0px 17px',
+    
     
   }
   
@@ -83,7 +90,7 @@ export function ImportList(props: ImportListProps) {
   return (
     <div style={style}>
       <div style={toggleDivStyle}>
-
+      <img src={foldIcon} style={toggleIconStyle}/>
       </div>
       <div style={importListStyle}>
         
