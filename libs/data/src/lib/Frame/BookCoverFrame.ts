6242348257b2f98006df9bbc1163a85556abe970
Y. C. Zhang <b04902047@csie.ntu.edu.tw>
2021-03-10 17:07:51 +0800

    libs/data: Frame/BookCoverFrame.ts

--- libs/data/src/lib/Frame/BookCoverFrame.ts
+++ libs/data/src/lib/Frame/BookCoverFrame.ts
@@ -2,5 +2,5 @@ import BleededRectangleFrame from "./BleededRectangleFrame";
 import Line from "./Line";
 
 export default abstract class BookCoverFrame extends BleededRectangleFrame {
-    protected abstract createFoldLines(): Line[];
+    public abstract foldLines: Array<Line>;
 }
