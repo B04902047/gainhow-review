ef865aa6c86bef9a90a26582a032f152124ce6da
黃韻儒 <lalame888@gmail.com>
2021-03-16 16:13:44 +0800

    增加 app/web-server

---
+++ apps/web-server/src/main.ts
@@ -0,0 +1,28 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import * as express from "express";
+
+const app = express();
+
+//  app.get("/reviewer", (req, res) => {
+//    let a= req.get('url');
+//   console.log(a);
+//   res.send('YA')
+//  });
+
+
+const dirUrl = '/Users/lalame888/projects/gainhow-review/dist/apps/';
+app.use('/reviewer',express.static(dirUrl+'reviewer'));
+app.use('/RA',express.static(dirUrl+'RA'));
+//app.use('/',express.static(dirUrl+'reviewer'));
+
+
+
+const port = process.env.port || 4300;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/reviewer`);
+});
+server.on("error", console.error);
